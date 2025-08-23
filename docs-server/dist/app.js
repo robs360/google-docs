4f546fdedb3658743394a0b6dc5bc166007900f7
shahadat <nayemshahadat581@gmail.com>
2025-08-23 12:49:19 +0600

    cors origin changed

--- docs-server/dist/app.js
+++ docs-server/dist/app.js
@@ -10,7 +10,7 @@ const routes_1 = __importDefault(require("./routes"));
 exports.app = (0, express_1.default)();
 exports.app.use(express_1.default.json());
 exports.app.use((0, cors_1.default)({
-    origin: ['http://localhost:3000', 'https://job-task-client-two.vercel.app'], // only allow local frontend
+    origin: ['http://localhost:3000', 'https://docs-hub-rosy.vercel.app'], // only allow local frontend
     credentials: true
 }));
 exports.app.use('/api/v1', routes_1.default);
--- docs-server/src/app.ts
+++ docs-server/src/app.ts
@@ -6,7 +6,7 @@ export const app: Application = express()
 
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://job-task-client-two.vercel.app'],  // only allow local frontend
+  origin: ['http://localhost:3000', 'https://docs-hub-rosy.vercel.app'],  // only allow local frontend
   credentials: true
 }));
 
